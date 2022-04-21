cdab62ae260662cdc1401dbb306855df45796cea
praksharma <1915438@swansea.ac.uk>
2022-04-21 22:18:14 +0100

    Some command updates

--- Jupyter_lab_port_forwarding/login_node.sh
+++ Jupyter_lab_port_forwarding/login_node.sh
@@ -1,1 +1,6 @@
+For lustrehome partition
 ssh -L 8888:localhost:8888 -t s.1915438@sunbird.swansea.ac.uk "module load anaconda/3;source activate ml;jupyter-lab"
+
+For scratch partition:
+
+ssh -L 8888:localhost:8888 -t s.1915438@sunbird.swansea.ac.uk "cd /scratch/s.1915438/;module load anaconda/3;source activate ml;jupyter-lab"
