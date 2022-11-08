e15b90133b73dcae9840b8711d6961dc98e538c0
Prakhar Sharma <1915438@swansea.ac.uk>
2022-11-08 11:15:21 +0000

    Ignoring python highlighting

--- docs/conf.py
+++ docs/conf.py
@@ -52,3 +52,5 @@ html_theme = 'furo'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+suppress_warnings = ['misc.highlighting_failure']
